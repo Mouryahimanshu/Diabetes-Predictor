d450471fecb7f38f18e0b14a30a9ba21ec568028
Himanshu Mourya <144560996+Mouryahimanshu@users.noreply.github.com>
2024-08-17 17:24:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,4 @@
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score
-
-# loading the diabetes dataset to a pandas DataFrame
-diabetes_dataset = pd.read_csv('/content/drive/MyDrive/project/diabetes.csv') 
-# printing the first 5 rows of the dataset
-diabetes_dataset.head()
-# separating the data and labels
-X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
-Y = diabetes_dataset['Outcome']
-scaler = StandardScaler()
-scaler.fit(X)
-standardized_data = scaler.transform(X)
-X = standardized_data
-Y = diabetes_dataset['Outcome']
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
-classifier = svm.SVC(kernel='linear')
-#training the support vector Machine Classifier
-classifier.fit(X_train, Y_train)
-# accuracy score on the training data
-X_train_prediction = classifier.predict(X_train)
-training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
-# accuracy score on the test data
-X_test_prediction = classifier.predict(X_test)
-test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
-
-import numpy as np
 import pickle
 import streamlit as st
 
